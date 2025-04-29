e03d8ab5f228747dfc04fba551bd1c3c99513948
Natalia Kaczyńska <natalia.kaczynska.programista@gmail.com>
2025-04-29 21:37:30 +0200

    add files after setting taks up for a time period

--- index.js
+++ index.js
@@ -23,24 +23,120 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
+function formatDate(date){
+  const newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + 1); //because automatic change of time 
+  // zone while retriving a date from the database
+  return newDate.toLocaleDateString('sv-SE', { timeZone: 'UTC' }); 
+}
+
+function setFirstWeekDay(today){
+  var difference = today.getDate() - today.getDay() + (today.getDay() === 0 ? -6 : 1);
+  return new Date(today.setDate(difference));
+}
+
+function setLastWeekDay(today){
+  var difference = today.getDate() - today.getDay() + (today.getDay() === 0 ? -6 : 1) + 1;
+  return new Date(today.setDate(difference));
+}
+
+function setFirstMonthDay(today){
+  return new Date(today.getFullYear(), today.getMonth(), 1);
+}
+
+function setLastMonthDay(today){
+  return new Date(today.getFullYear(), today.getMonth() + 1, 0);
+}
+
 app.get("/", async (req, res) => {
   try{
-    const result = await db.query("SELECT * FROM items ORDER id BY ASC");
-    items = result.rows;
+    const today = new Date().toLocaleDateString('sv-SE', { timeZone: 'UTC' });
+
+    const result = await db.query("SELECT * FROM items ORDER BY date ASC");
+    const items = result.rows.map((item) => {
+      item.date = formatDate(item.date);
+      return item;
+    });
     res.render("index.ejs", {
-      listTitle: "Today",
+      listTitle: "To-do-list",
       listItems: items,
+      currentDate: today,
+      selectedPeriod: null
     });
 } catch(err){
   console.log(err);
 }
 });
 
+app.post("/timeSlot", async (req, res) => {
+  const today = new Date().toLocaleDateString('sv-SE', { timeZone: 'UTC' });
+  const todayWithoutSettingLocalTime = new Date();
+  const selectedPeriod = req.body.timeSlot;
+
+  try{
+    let result = "";
+    let items = "";
+    let startDay = "";
+    let lastDay = "";
+      if(selectedPeriod){
+        switch(selectedPeriod){
+          case 'today':
+            result = await db.query("SELECT * FROM items WHERE date = $1 ORDER BY date ASC", [today]);
+            items = result.rows.map((item) => {
+              item.date = formatDate(item.date);
+              return item;
+            });
+            return res.render("index.ejs", {
+              listTitle: "To-do-list",
+              listItems: items,
+              currentDate: today,
+              selectedPeriod: selectedPeriod
+            });
+          case 'current week':
+            startDay = setFirstWeekDay(todayWithoutSettingLocalTime);
+            lastDay = setLastWeekDay(todayWithoutSettingLocalTime);
+            result = await db.query("SELECT * FROM items WHERE date BETWEEN ($1) AND ($2) ORDER BY date ASC", [startDay, lastDay]);
+            items = result.rows.map((item) => {
+              item.date = formatDate(item.date);
+              return item;
+            });
+            return res.render("index.ejs", {
+              listTitle: "To-do-list",
+              listItems: items,
+              currentDate: today,
+              selectedPeriod: selectedPeriod
+            });
+          case 'current month':
+            startDay = setFirstMonthDay(todayWithoutSettingLocalTime);
+            lastDay = setLastMonthDay(todayWithoutSettingLocalTime);
+            result = await db.query("SELECT * FROM items WHERE date BETWEEN ($1) AND ($2) ORDER BY date ASC", [startDay, lastDay]);
+            items = result.rows.map((item) => {
+              item.date = formatDate(item.date);
+              return item;
+            });
+            return res.render("index.ejs", {
+              listTitle: "To-do-list",
+              listItems: items,
+              currentDate: today,
+              selectedPeriod: selectedPeriod
+            });
+          default:
+            return res.send("Incorrect choice.");
+        }
+    }
+    res.redirect("/");
+  } catch (err) {
+      console.log(err);
+      res.status(500).send("Error retrieving data.");
+    }
+})
+
 app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
+  const date = req.body.date;
+  items.push({ title: item, date: date });
   try{
-    await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
+    await db.query("INSERT INTO items (title, date) VALUES ($1, $2)", [item, date]);
     res.redirect("/");
   } catch(err){ 
     console.log(err);
